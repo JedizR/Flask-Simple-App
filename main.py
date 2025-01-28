fe56a6ed250c545a132f27bb9e397760b73ee297
JedizR <teenday.jedi@gmail.com>
2025-01-29 04:05:25 +0700

    sum of square of 2 numbers in url

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/test/<int:num_1>/<int:num_2>")
+def show_squared_num_from_url(num_1, num_2):
+    return f"({int(num_1)}^2) + ({int(num_2)}^2) = {int(num_1)**2 + int(num_2)**2}</p>"
